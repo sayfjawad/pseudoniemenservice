66a1c59347ef76a9045f49916f2526f178af4cb5
Bas Huisman <bash@lostlemon.nl>
2024-11-04 14:50:08 +0100

    add ExchangeIdentifier controller

--- src/main/java/nl/ictu/controller/ExchangeIdentifier.java
+++ src/main/java/nl/ictu/controller/ExchangeIdentifier.java
@@ -5,13 +5,13 @@ import nl.ictu.psuedoniemenservice.generated.server.model.WsGetTokenRequest;
 import nl.ictu.psuedoniemenservice.generated.server.model.WsIdentifier;
 import nl.ictu.psuedoniemenservice.generated.server.model.WsIdentifierTypes;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ExchangeIdentifier implements ExchangeIdentifierApi {
     @Override
     public ResponseEntity<WsIdentifier> exchangeIdentifierForIdentifier(WsGetTokenRequest wsGetTokenRequest) {
-        final WsIdentifier wsIdentifier = new WsIdentifier()
-            .identifierType(WsIdentifierTypes.BSN)
-            .identifierValue("476288216");
+        final WsIdentifier wsIdentifier = new WsIdentifier().identifierType(WsIdentifierTypes.BSN).identifierValue("476288216");
 
         return ResponseEntity.ok(wsIdentifier);
     }
