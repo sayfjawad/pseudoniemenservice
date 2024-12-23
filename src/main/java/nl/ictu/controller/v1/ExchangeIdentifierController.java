b22f4ef6d848c087e89665fb0201c2e08baf38b9
sayf <sayf.jawad@alliander.com>
2024-12-23 21:06:46 +0100

    More refactoring,  split responsibilities

--- src/main/java/nl/ictu/controller/v1/ExchangeIdentifierController.java
+++ src/main/java/nl/ictu/controller/v1/ExchangeIdentifierController.java
@@ -4,25 +4,23 @@ import static nl.ictu.pseudoniemenservice.generated.server.model.WsIdentifierTyp
 import static nl.ictu.pseudoniemenservice.generated.server.model.WsIdentifierTypes.ORGANISATION_PSEUDO;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import nl.ictu.Identifier;
 import nl.ictu.pseudoniemenservice.generated.server.api.ExchangeIdentifierApi;
 import nl.ictu.pseudoniemenservice.generated.server.model.WsExchangeIdentifierRequest;
 import nl.ictu.pseudoniemenservice.generated.server.model.WsExchangeIdentifierResponse;
-import nl.ictu.pseudoniemenservice.generated.server.model.WsIdentifier;
-import nl.ictu.service.v1.crypto.AesGcmSivCryptographer;
-import org.bouncycastle.crypto.InvalidCipherTextException;
+import nl.ictu.service.v1.map.BsnPseudoMapper;
+import nl.ictu.service.v1.map.PseudoBsnMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-public final class ExchangeIdentifierController implements ExchangeIdentifierApi, VersionOneController {
+public final class ExchangeIdentifierController implements ExchangeIdentifierApi,
+        VersionOneController {
 
-    public static final String V_1 = "v1";
-    private final AesGcmSivCryptographer aesGcmSivCryptographer;
+    private final BsnPseudoMapper bsnPseudoMapper;
+    private final PseudoBsnMapper pseudoBsnMapper;
 
     @Override
     @SneakyThrows
@@ -34,40 +32,15 @@ public final class ExchangeIdentifierController implements ExchangeIdentifierApi
         final var recipientIdentifierType = wsExchangeIdentifierForIdentifierRequest.getRecipientIdentifierType();
         if (BSN.equals(wsIdentifierRequest.getType()) && ORGANISATION_PSEUDO.equals(
                 recipientIdentifierType)) {
-            // from BSN to Org Pseudo
             return ResponseEntity.ok(
-                    mapBsnToPseudo(wsIdentifierRequest.getValue(), recipientOIN));
+                    bsnPseudoMapper.map(wsIdentifierRequest.getValue(), recipientOIN));
         } else if (ORGANISATION_PSEUDO.equals(wsIdentifierRequest.getType()) && BSN.equals(
                 recipientIdentifierType)) {
-            // from BSN to Org Pseudo
             return ResponseEntity.ok(
-                    mapPseudoToBEsn(wsIdentifierRequest.getValue(), recipientOIN));
+                    pseudoBsnMapper.map(wsIdentifierRequest.getValue(), recipientOIN));
         }
         return ResponseEntity.status(UNPROCESSABLE_ENTITY).build();
     }
 
-    private WsExchangeIdentifierResponse mapBsnToPseudo(final String bsn, final String oin)
-            throws IOException, InvalidCipherTextException {
 
-        return WsExchangeIdentifierResponse.builder()
-                .identifier(WsIdentifier.builder()
-                        .type(ORGANISATION_PSEUDO)
-                        .value(aesGcmSivCryptographer.encrypt(Identifier.builder()
-                                .version(V_1)
-                                .bsn(bsn)
-                                .build(), oin))
-                        .build())
-                .build();
-    }
-
-    private WsExchangeIdentifierResponse mapPseudoToBEsn(final String pseudo, final String oin)
-            throws IOException, InvalidCipherTextException {
-
-        return WsExchangeIdentifierResponse.builder()
-                .identifier(WsIdentifier.builder()
-                        .type(BSN)
-                        .value(aesGcmSivCryptographer.decrypt(pseudo, oin).getBsn())
-                        .build())
-                .build();
-    }
 }
--- src/main/java/nl/ictu/service/v1/map/BsnPseudoMapper.java
+++ src/main/java/nl/ictu/service/v1/map/BsnPseudoMapper.java
@@ -1,9 +1,44 @@
-/*
 package nl.ictu.service.v1.map;
 
+import static nl.ictu.pseudoniemenservice.generated.server.model.WsIdentifierTypes.ORGANISATION_PSEUDO;
+
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import nl.ictu.Identifier;
+import nl.ictu.pseudoniemenservice.generated.server.model.WsExchangeIdentifierResponse;
+import nl.ictu.pseudoniemenservice.generated.server.model.WsIdentifier;
+import nl.ictu.service.v1.crypto.AesGcmSivCryptographer;
+import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class BsnPseudoMapper {
 
+    public static final String V_1 = "v1";
+    private final AesGcmSivCryptographer aesGcmSivCryptographer;
 
+    /**
+     * Maps a given BSN (Burger Service Nummer) and OIN (Organisatie-identificatienummer) to a
+     * {@link WsExchangeIdentifierResponse} containing a pseudo-anonymous identifier.
+     *
+     * @param bsn the BSN to be encrypted and included in the identifier
+     * @param oin the OIN used as the salt for encryption
+     * @return a {@link WsExchangeIdentifierResponse} containing the pseudo-anonymous identifier
+     * @throws IOException if an I/O error occurs during the encryption process
+     * @throws InvalidCipherTextException if encryption fails due to invalid cipher text
+     */
+    public WsExchangeIdentifierResponse map(final String bsn, final String oin)
+            throws IOException, InvalidCipherTextException {
 
+        return WsExchangeIdentifierResponse.builder()
+                .identifier(WsIdentifier.builder()
+                        .type(ORGANISATION_PSEUDO)
+                        .value(aesGcmSivCryptographer.encrypt(Identifier.builder()
+                                .version(V_1)
+                                .bsn(bsn)
+                                .build(), oin))
+                        .build())
+                .build();
+    }
 }
-*/
